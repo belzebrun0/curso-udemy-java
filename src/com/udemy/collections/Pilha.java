14ba9dcb5787649fba15cf840d2cb889fdbd2c93
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2019-04-03 21:31:51 -0400

    Criado a classe Pilha

---
+++ src/com/udemy/collections/Pilha.java
@@ -0,0 +1,24 @@
+package com.udemy.collections;
+
+import java.util.Stack;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		Stack<String> pilha = new Stack<>();
+		
+		pilha.push("O Pequeno Príncipe");
+		pilha.push("O Hobbit");
+		pilha.push("Don Quixote");
+		
+		System.out.println("Peek");
+		System.out.println(pilha.peek());
+		
+		System.out.println("Poop");
+		System.out.println(pilha.pop());
+		System.out.println(pilha.pop());
+		System.out.println(pilha.pop());
+
+	}
+
+}
